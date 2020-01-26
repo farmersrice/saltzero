b662be54623d0d01c26e5d46d4640706f467189d
farmersrice <ansonhu@gmail.com>
2020-01-26 12:36:50 -0800

    add lower learning rate script

---
+++ lower_learning_rate.py
@@ -0,0 +1,34 @@
+from TrainingManager import TrainingManager
+from NeuralNet import NeuralNet
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # no gpu.
+
+import sys
+
+import tensorflow as tf
+
+conf = tf.compat.v1.ConfigProto()
+conf.gpu_options.per_process_gpu_memory_fraction=0.6
+session = tf.compat.v1.Session(config=conf)
+
+tf.compat.v1.disable_eager_execution()
+
+
+learning_rate = float(sys.argv[1])
+
+temp_manager = TrainingManager()
+
+
+model = temp_manager.net.net.model
+
+import tensorflow.keras.backend as K 
+
+print("Current learning rate: " + str(K.get_value(model.optimizer.lr)))
+print("Setting learning rate to " + str(learning_rate))
+K.set_value(model.optimizer.lr, learning_rate)
+
+print("Done")
+
+
+model.save("models/best_0.h5")
